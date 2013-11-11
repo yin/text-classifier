46a0febd400df3150c66f182217f4ec7c5239d0f
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2013-11-11 19:58:28 +0100

    Refactored all paths in make_wiki script.

--- wikipedia-dumps/scripts/make_data-wikipedia_dumps.sh
+++ wikipedia-dumps/scripts/make_data-wikipedia_dumps.sh
@@ -1,31 +1,59 @@
 #!/bin/bash
 
+download_date=20131006
+source_bz2_dir="data-01packed"
+source_bz2_name="skwiki-$download_date-pages-articles.xml.bz2"
+source_bz2="$source_bz2_dir/skwiki-$download_date-pages-articles.xml.bz2"
+source_bz2_url="http://dumps.wikimedia.org/skwiki/$download_date/skwiki-$download_date-pages-articles.xml.bz2"
+source_file_dir="data-02unpacked"
+source_file_name=${source_bz2_name%.bz2}
+source_file="$source_file_dir/$source_file_name"
+splits_dir="data-04splits"
+wikiout_dir="data-10wikiout_pseudo_xml"
+wikitext_dir="data-11wiki_plain_text"
+wikimegadoc_dir="data-12wiki_megadoc"
+
+wiki_extractor="python tools/WikiExtractor.py"
+wiki_extractor_mt="python tools/WikiExtractor-mt2.py"
+
+mt=false
+
 # Download source
-mkdir -p source/
-source_bz2=source/skwiki-20131006-pages-articles.xml.bz2
-wget_opts=""
-if [ -w $source_bz2 ]; then
-	wget_opts="$wget_opts -c"
+if [ ! -f $source_bz2 ]; then
+	mkdir -p $source_bz2_dir
+	wget_opts=""
+	if [ -w $source_bz2 ]; then
+		wget_opts="$wget_opts -c"
+	fi
+	wget $source_bz2_url \
+		$wget_opts -O $source_bz2
+else
+	echo "... the wikipedia pack is downloaded."
 fi
-wget http://dumps.wikimedia.org/skwiki/20131006/skwiki-20131006-pages-articles.xml.bz2 \
-	$wget_opts -O $source_bz2
 
 # Preprocess
-unpack=${source_bz2%.bz2}
-if [ ! -f $unpack ]; then
+if [ ! -f $source_file ]; then
 	echo unpacking
-	bzcat $source_bz2 > $unpack
+	mkdir $source_file_dir
+	bzcat $source_bz2 > $source_file
 fi
-if [ ! -d splits/ ]; then 
-	echo splitting
-	mkdir -p splits/
-	cat $unpack \
-		| ( cd splits; csplit -f 'skwiki' -b '%06d.xml' - '/<page>/' '{*}' ) \
+# For my XML-wellformness imune MT wikiextractor, I need to split the BIG XML. 
+if [ ! -d splits/ ] && [ "$mt" == true ]; then 
+	echo splitting mt=true
+	mkdir -p $splits_dir
+	# Split the BIG XML into files on lines containing <page> tag
+	cat $source_file \
+		| ( cd $splits_dir; csplit -f 'skwiki' -b '%06d.xml' - '/<page>/' '{*}' ) \
 		| awk 'BEGIN{N=0} {N=N+1} N%1000==0{printf "Processed: " N "\n"} N%1000==500{printf "\r"}'
 fi
 
-# wiki-markup2txt
-echo textizing
-mkdir -p wikiout/
-python tools/WikiExtractor.py -o wikiout/ < $unpack
+# Convert wikimedia markup format to plain/text
+if [ ! -d $wikiout_dir ] && [ "$mt" == false ]; then 
+	echo textizing
+	mkdir -p $wikiout_dir
+	echo $wiki_extractor -o $wikiout_dir '<' $source_file
+	$wiki_extractor -o $wikiout_dir < $source_file
+else
+	echo "... already textized - $wikiout_dir present."
+fi
 
